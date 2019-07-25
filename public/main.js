df246d9188088626086d4decbc6f199eaa421f13
annemarleen <amholbosma@gmail.com>
2019-07-25 17:04:26 +0200

    MediCall app afronden1.0

--- public/main.js
+++ public/main.js
@@ -27,20 +27,11 @@ function showValue(id, value) {
   document.getElementById(id).textContent = value;
 }
 
-function onload() {
-  var container = document.getElementById("buttonContainer");
-  container.style.display = "none";
-  container = document.getElementById("statusContainer");
-  container.style.display = "none";
-  weerUpdate();
-  //pollweer();
-}
-
 function chooseMed() {
   const medication1 = document.getElementById("chooseMed");
   console.log(medication1);
 }
-
+// index
 function ENclick() {
   const NLvlag = document.getElementById("NLvlag");
   NLvlag.style.display = "block";
@@ -51,6 +42,7 @@ function ENclick() {
   introP.innerText =
     "Not feeling right? This could be from one of the following medication. Please click one to check.";
 }
+//index
 function NLclick() {
   const NLvlag = document.getElementById("NLvlag");
   NLvlag.style.display = "none";
@@ -85,20 +77,23 @@ function bodyOnload() {
       "Voel jij je niet helemaal lekker? Dit zou een bijwerking van een medicijn kunnen zijn:";
   }
 }
+//checks if the medication is responsible for the side effect or not
 function checkMed() {
   const invoerInput = document.getElementById("invoerKlacht").value;
   var item;
+  const arrayAzathioprine = [
+    "bloedarmoede”, “bloedingen”, “infecties”, “vermoeidheid”, “bloedneus”, “keelpijn”, “koorts”, “misselijkheid”, “braken”, “haaruitval”, “huidkanker”, “PML”, “spierzwakte”, “overgevoeligheid”, “jeuk”, “galbulten”, “koorts”, “benauwdheid”, “flauwvallen”, “huidaandoening”, “leveraandoening"
+  ];
   if (item in arrayAzathioprine != invoerInput) {
     console.log("yes");
-
-    //medicatieLijstTextArea = document.getElementById("medicatieLijst")
-    // .innerHTML;
   } else {
     console.log("Noooooooooooooo!");
   }
 }
 
 function submitCheckOk() {
+  const container = document.getElementById("resultatenLijst");
+  container.style.display = "block";
   const invoerInput = document.getElementById("invoerKlacht").value;
   if (invoerInput) {
     //var containerDiv = document.getElementById("container");
@@ -110,37 +105,4 @@ function submitCheckOk() {
   } else {
     alert("Vul een kwaal in.");
   }
-}
-
-function drieOpEenRij(buttonid1, buttonid2, buttonid3) {
-  return (
-    document.getElementById(buttonid1).textContent == symbool &&
-    document.getElementById(buttonid2).textContent == symbool &&
-    document.getElementById(buttonid3).textContent == symbool
-  );
-}
-
-function veldenVol() {
-  if (!document.getElementById("b1").disabled) return false;
-  if (!document.getElementById("b2").disabled) return false;
-  if (!document.getElementById("b3").disabled) return false;
-  if (!document.getElementById("b4").disabled) return false;
-  if (!document.getElementById("b5").disabled) return false;
-  if (!document.getElementById("b6").disabled) return false;
-  if (!document.getElementById("b7").disabled) return false;
-  if (!document.getElementById("b8").disabled) return false;
-  if (!document.getElementById("b9").disabled) return false;
-  return true;
-}
-
-function wisselSpeler() {
-  if (spelerAanDeBeurt == document.getElementById("player1").value) {
-    spelerAanDeBeurt = document.getElementById("player2").value;
-    symbool = "O";
-  } else {
-    spelerAanDeBeurt = document.getElementById("player1").value;
-    symbool = "X";
-  }
-  document.getElementById("huidigeSpeler").textContent = spelerAanDeBeurt;
-  document.getElementById("symbool").textContent = symbool;
 }
